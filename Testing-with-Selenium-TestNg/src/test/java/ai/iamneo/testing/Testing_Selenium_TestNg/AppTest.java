8db2b765cbedfb1ca6a0caa622a9e1a03d9017be
Iamneo <techadmin@iamneo.ai>
2023-08-08 15:21:06 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -32,15 +32,15 @@ public void beforeTest() throws Exception {
 	public void Iamneo() throws InterruptedException{
 	
 		driver.get("https://www.flipkart.com/");
-
+		WebElement signin = driver.findElement(By.cssSelector("#container > div > div.q8WwEU > div > div > div > div > div.css-1dbjc4n.r-13awgt0 > div > div.css-1dbjc4n.r-13awgt0.r-1iqfa7g.r-60vfwk > div > div._2NhoPJ > header > div._2msBFL > div:nth-child(2) > div > div > div > a"));
+		WebElement emailField = driver.findElement(By.cssSelector("#container > div > div._2dSUjN > div > div._36HLxm.col.col-3-5 > div > form > div.IiD88i._351hSN > input"));
+		emailField.sendKeys("abcde@gmail.com");
 	}
 	
 	@Test
 	public void NextPage() throws InterruptedException {
 		//String str="abcde@gmail.com";
-		WebElement signin = driver.findElement(By.cssSelector("#container > div > div.q8WwEU > div > div > div > div > div.css-1dbjc4n.r-13awgt0 > div > div.css-1dbjc4n.r-13awgt0.r-1iqfa7g.r-60vfwk > div > div._2NhoPJ > header > div._2msBFL > div:nth-child(2) > div > div > div > a"));
-		WebElement emailField = driver.findElement(By.cssSelector("#container > div > div._2dSUjN > div > div._36HLxm.col.col-3-5 > div > form > div.IiD88i._351hSN > input"));
-		emailField.sendKeys("abcde@gmail.com");
+		
 		
 		WebElement requestButton = driver.findElement(By.cssSelector("body > div._2Sn47c > div > div > div > div > div._36HLxm.col.col-3-5 > div > form > div._1k3JO2 > button"));
 		requestButton.click();
